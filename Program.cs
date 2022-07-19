4d81b923fffa2b951cc9a2c210d594bf4475d78b
VictorCosta2002 <victorcosta123vcs@gmail.com>
2022-07-19 17:55:11 -0300

    Usando um pouco de POO

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autocarro.Src;
+
+namespace Autocarro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tipos astra = new Tipos("Astra", 0, 42590);
+            Tipos mode = new Tipos("Gol", 1, 50000);
+            Tipos oi = new Tipos("Up", 2, 39087);
+
+
+            Console.WriteLine(astra.ToString());
+            Console.WriteLine("<3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3");
+            Console.WriteLine(mode.ToString());
+            Console.WriteLine("<3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 <3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3");
+            Console.WriteLine(oi.ToString());
+            Console.WriteLine("<3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3 < 3");
+        }
+    }
+}
---
+++ Tipos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocarro.Src
+{
+    internal class Tipos
+    {
+        public string Carro { get; set; }
+
+        public int Tipo { get; set; }
+
+        public int Preco { get; set; }
+
+        public Tipos(string carro, int tipo, int preco)
+        {
+            Carro = carro;
+            Tipo = tipo;
+            Preco = preco;
+
+        }
+        public override string ToString()
+        {
+            return "O carro é um: " + Carro +"\n tipo do carro: "+ Tipo +"\n Preço do carro : " + Preco ;
+        }
+
+
+    }
+}
